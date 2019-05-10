0909fcba7a33130ce6c2a2a1c121615635963ccd
SamShoc <7794501@isd535.org>
2019-05-10 09:01:47 -0500

    got through the first couple steps of the tutorial an dtheirs a couple of bug in the toast and the on click method

--- app/src/main/java/com/ctech/shockman/productfinder/ProductFragment.java
+++ app/src/main/java/com/ctech/shockman/productfinder/ProductFragment.java
@@ -10,6 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 //https://code.tutsplus.com/tutorials/android-sdk-create-a-barcode-reader--mobile-17162
 //https://github.com/SueSmith/android-book-barcode-scanner
@@ -31,9 +37,30 @@ public class ProductFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_product, container, false);
 
-        mPicButton = v.findViewById(R.id.pic_button);
         mTextButton = v.findViewById(R.id.text_button);
+        mPicButton = v.findViewById(R.id.pic_button);
+        mPicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v.getId()==R.id.pic_button) {
+                    IntentIntegrator scanIntegrator = new IntentIntegrator();
+                    scanIntegrator.initiateScan();
+                }
+            }
+        });
+
 
         return v;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+        if (scanningResult != null) {
+
+        } else {
+            Toast toast = Toast.makeText(R.string.no_data);
+            toast.show();
+        }
     }
 }
