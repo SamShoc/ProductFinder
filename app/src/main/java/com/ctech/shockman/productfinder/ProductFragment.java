3358acf11c24fc309e4ba1459c4cb455e7e5bc3e
SamShoc <7794501@isd535.org>
2019-05-16 08:24:51 -0500

    Started work on adding a button to mannually search barcode

--- app/src/main/java/com/ctech/shockman/productfinder/ProductFragment.java
+++ app/src/main/java/com/ctech/shockman/productfinder/ProductFragment.java
@@ -1,27 +1,22 @@
 package com.ctech.shockman.productfinder;
 
 import android.app.SearchManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.View.OnClickListener;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
 
 //https://code.tutsplus.com/tutorials/android-sdk-create-a-barcode-reader--mobile-17162
 //https://github.com/SueSmith/android-book-barcode-scanner
@@ -31,8 +26,9 @@ import java.util.List;
 public class ProductFragment extends Fragment {
 
     private Button mPicButton;
-    private Button mTextButton;
+    private EditText mTextBox;
     private TextView mContentText;
+    private Button mEnterButton;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,13 +42,23 @@ public class ProductFragment extends Fragment {
         }
     }
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_product, container, false);
 
-        mTextButton = v.findViewById(R.id.text_button);
+        mTextBox = v.findViewById(R.id.text_box);
         mContentText = v.findViewById(R.id.scan_content);
+
+        mEnterButton = v.findViewById(R.id.enter_button);
+        mEnterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         mPicButton = v.findViewById(R.id.pic_button);
         mPicButton.setOnClickListener(new View.OnClickListener() {
             @Override
